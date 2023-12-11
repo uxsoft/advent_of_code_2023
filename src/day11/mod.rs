76e48f554962b4e2b45812ea78e8b5ab44dd00d1
Dryk, Jan <jan.dryk@merck.com>
2023-12-11 10:36:09 +0100

    day 11

---
+++ src/day11/mod.rs
@@ -0,0 +1,129 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+pub fn parse(input: &str) -> HashSet<(usize, usize)> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().flat_map(move |(x, c)| match c {
+                '#' => Some((x, y)),
+                '.' => None,
+                c => panic!("Unexpected char in input {c}"),
+            })
+        })
+        .collect()
+}
+
+pub fn part1(input: &str) -> usize {
+    part2(input, 2)
+}
+
+fn part2(input: &str, factor: usize) -> usize {
+    let galaxies = parse(input);
+    let dim_y = input.lines().count();
+    let dim_x = input.lines().nth(0).unwrap().chars().count();
+
+    let expansions_y = (0..dim_y)
+        .filter(|y| !galaxies.iter().any(|(_, gy)| gy == y))
+        .collect_vec();
+    let expansions_x = (0..dim_x)
+        .filter(|x| !galaxies.iter().any(|(gx, _)| gx == x))
+        .collect_vec();
+
+    println!("Expansions: {:?} + {:?}", expansions_x, expansions_y);
+
+    let expanded_galaxies = galaxies
+        .iter()
+        .map(|(gx, gy)| {
+            let ey = expansions_y.iter().filter(|y| gy > y).count() * (factor - 1);
+            let ex = expansions_x.iter().filter(|x| gx > x).count() * (factor - 1);
+            (gx + ex, gy + ey)
+        })
+        .collect::<HashSet<_>>();
+
+    let distances = expanded_galaxies
+        .iter()
+        .tuple_combinations::<(_, _)>()
+        .map(|((ax, ay), (bx, by))| {
+            let distance = ax.abs_diff(*bx) + ay.abs_diff(*by);
+            println!("Distance between {ax},{ay} and {bx},{by} : {distance}");
+            return distance;
+        })
+        .sum();
+
+    return distances;
+}
+
+pub fn process(input: String) {
+    let result = part2(&input, 1_000_000);
+    println!("Result: {result}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+        let result = part1(input);
+        assert_eq!(result, 374);
+    }
+
+    #[test]
+    fn part1_input() {
+        let input = include_str!("input.txt");
+        let result = part1(input);
+        assert_eq!(result, 9681886);
+    }
+
+    #[test]
+    fn part2_example1() {
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+        let result = part2(input, 10);
+        assert_eq!(result, 1030);
+    }
+
+    #[test]
+    fn part2_example2() {
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+        let result = part2(input, 100);
+        assert_eq!(result, 8410);
+    }
+
+    #[test]
+    fn part2_input() {
+        let input = include_str!("input.txt");
+        let result = part2(input, 1_000_000);
+        assert_eq!(result, 791134099634);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::{env, fs, path::Path};
 
-mod day7;
+mod day11;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let default_filename = Path::new("src/day7/sharp.txt");
+    let _args: Vec<String> = env::args().collect();
+    let default_filename = Path::new("src/day11/input.txt");
     // let filename = args.get(1).unwrap_or(&default_filename);
 
     let input = fs::read_to_string(default_filename).expect("Wrong file location");
 
-    day7::process(input);
+    day11::process(input);
 }
