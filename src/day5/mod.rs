df451955a07c147901af73c813833ef840961762
Jan Dryk <jan.dryk@merck.com>
2023-12-14 22:52:56 +0100

    Refactor/day5 (#1)
    * parse differently
    
    * day5 fix parsing
    
    * add offset fn
    
    * finish day 5 refactoring

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,4 +1,4 @@
-use rayon::prelude::*;
+use itertools::Itertools;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -22,17 +22,25 @@ struct Mapping {
 
 impl Mapping {
     pub fn parse(input: &str) -> Mapping {
-        let split: Vec<i64> = input
-            .split(" ")
-            .map(str::parse)
-            .map(Result::unwrap)
-            .collect();
+        let split: Vec<i64> = input.split(" ").map(str::parse).flatten().collect();
 
         Mapping {
             destination_start: *split.get(0).unwrap(),
             source_start: *split.get(1).unwrap(),
             length: *split.get(2).unwrap(),
         }
+    }
+
+    fn destination_end(&self) -> i64 {
+        self.destination_start + self.length - 1
+    }
+
+    fn source_end(&self) -> i64 {
+        self.source_start + self.length - 1
+    }
+
+    fn offset(&self) -> i64 {
+        self.source_start - self.destination_start
     }
 }
 
@@ -43,8 +51,12 @@ struct Projection {
 
 impl Projection {
     pub fn parse(input: &str) -> Projection {
-        let mut mappings: Vec<Mapping> = input.lines().map(Mapping::parse).collect();
-        mappings.sort_by(|a, b| a.source_start.partial_cmp(&b.source_start).unwrap());
+        let mappings: Vec<Mapping> = input
+            .lines()
+            .skip(1)
+            .map(Mapping::parse)
+            .sorted_by_key(|m| m.source_start)
+            .collect();
 
         Projection { mappings }
     }
@@ -71,7 +83,7 @@ impl Projection {
             if let Some(mapping) = self
                 .mappings
                 .iter()
-                .find(|m| i < m.source_start + m.length)
+                .find(|m| i < m.source_start + m.length - 1)
             {
                 if i >= mapping.source_start {
                     // We are inside of the mapping, advance to the end of the mapping or as long as `l` goes
@@ -94,173 +106,119 @@ impl Projection {
             }
         }
 
-        println!("Mapped range {}-{} to {:?}", range.start, range.length, dest_ranges);
+        println!(
+            "Mapped range {}-{} to {:?}",
+            range.start, range.length, dest_ranges
+        );
         return dest_ranges;
     }
 
-    pub fn map_range_1(&self, range: &Range) -> Vec<Range> {
+    pub fn map_range_fast(&self, range: &Range) -> Vec<Range> {
         let mut dest_ranges = vec![];
 
         let mut i = range.start;
         let mut l = range.length;
 
-        for mapping in &self.mappings {
-            if i < mapping.source_start {
-                if i + l < mapping.source_start {
-                    // We are before the next mapping and shorter than to get to it
-                    // We return a range from i to l with no translation
-                    // And we're done!
-                    dest_ranges.push(Range::new(i, l));
-                    return dest_ranges;
-                } else {
-                    // We are before the next mapping and will go beyond it's start
-                    // Advance to mapping start
-                    let distance_to_start = mapping.source_start - i;
-
-                    dest_ranges.push(Range::new(i, distance_to_start));
-                    i += distance_to_start;
-                    l -= distance_to_start;
-
-                    // Consume from the next mapping
-                    let offset = i - mapping.source_start;
-                    if i + l < mapping.source_start + mapping.length {
-                        // Range ends inside of this mapping
-                        // Push mapped range, return
-                        dest_ranges.push(Range::new(mapping.destination_start + offset, l));
-                        return dest_ranges;
-                    } else {
-                        // Range ends after this mapping
-                        // Push mapped range until the end, continue;
-                        let count = i + l - mapping.source_start - mapping.length;
-                        dest_ranges.push(Range::new(mapping.destination_start + offset, count));
-                        i += count;
-                        l -= count;
-
-                        continue;
-                    }
-                }
-            } else {
-                // i >= mapping.start
-                if i > mapping.source_start + mapping.length {
-                    // This mapping ends before our range starts
-                    continue;
-                } else if i + l < mapping.source_start + mapping.length {
-                    // Range ends inside of this mapping
-                    // Push mapped range, return
-                    let offset = i - mapping.source_start;
-                    dest_ranges.push(Range::new(mapping.destination_start + offset, l));
-                    return dest_ranges;
-                } else {
-                    // Range ends after this mapping
-                    // Push mapped range until the end, continue;
-                    let offset = i - mapping.source_start;
-                    let count = i + l - mapping.source_start - mapping.length;
-                    dest_ranges.push(Range::new(mapping.destination_start + offset, count));
-                    i += count;
-                    l -= count;
-
-                    continue;
-                }
+        for m in &self.mappings {
+            if i < m.source_start && l > 0 {
+                // We are before this mapping
+                let range_length = l.min(m.source_start - i);
+                dest_ranges.push(Range::new(i, range_length));
+                i += range_length;
+                l -= range_length;
             }
+
+            if i < m.source_start + m.length && l > 0 {
+                // We are inside this mapping (start or middle)
+                let offset = i - m.source_start;
+
+                if offset < 0 {
+                    panic!("Offset is negative! i: {}, start: {}`", i, m.source_start);
+                }
+
+                let range_length = l.min(m.length - offset);
+                dest_ranges.push(Range::new(m.destination_start + offset, range_length));
+                i += range_length;
+                l -= range_length;
+            }
+        }
+
+        if l > 0 {
+            // We passed all the mappings and still have left
+            println!("Passed all mappings and we still have i left: {l}");
+            dest_ranges.push(Range::new(i, l));
+            i += l;
+            l -= l;
+        }
+
+        if i != range.start + range.length {
+            println!("map_range2 wrong size result: i: {i}, range: {range:?}")
         }
 
         return dest_ranges;
     }
 
     pub fn map_ranges(&self, ranges: Vec<Range>) -> Vec<Range> {
-        ranges.iter().flat_map(|r| self.map_range(r)).collect()
-    }
-}
-
-trait MappingChain {
-    fn map_to(&self, source: i64) -> i64;
-    fn map_ranges(&self, ranges: Vec<Range>) -> Vec<Range>;
-}
-
-impl MappingChain for Vec<&Projection> {
-    fn map_to(&self, source: i64) -> i64 {
-        let mut i = source;
-        for m in self {
-            i = m.map_to(i);
-        }
-        return i;
-    }
-
-    fn map_ranges(&self, ranges: Vec<Range>) -> Vec<Range> {
-        let mut i = ranges;
-        for m in self {
-            i = m.map_ranges(i);
-        }
-        return i;
+        ranges.iter().flat_map(|r| self.map_range_fast(r)).collect()
     }
 }
 
 #[derive(Debug)]
 struct Almanac {
     seeds: Vec<i64>,
-    seed_to_soil: Projection,
-    soil_to_fertilizer: Projection,
-    fertilizer_to_water: Projection,
-    water_to_light: Projection,
-    light_to_temperature: Projection,
-    temperature_to_humidity: Projection,
-    humidity_to_location: Projection,
+    projections: Vec<Projection>,
 }
 
 impl Almanac {
-    pub fn parse(input: String) -> Almanac {
-        let regex = Regex::new(r"seeds: ([\s\d]+)\r\n\r\nseed-to-soil map:\r\n([\s\d]+)\r\n\r\nsoil-to-fertilizer map:\r\n([\s\d]+)\r\n\r\nfertilizer-to-water map:\r\n([\s\d]+)\r\n\r\nwater-to-light map:\r\n([\s\d]+)\r\n\r\nlight-to-temperature map:\r\n([\s\d]+)\r\n\r\ntemperature-to-humidity map:\r\n([\s\d]+)\r\n\r\nhumidity-to-location map:\r\n([\s\d]+)").unwrap();
-        let captures = regex.captures(&input).unwrap();
+    pub fn parse(input: &str) -> Self {
+        let double_line_ending: Regex = Regex::new(r"\r?\n\r?\n").unwrap();
 
-        Almanac {
-            seeds: captures
-                .get(1)
-                .unwrap()
-                .as_str()
-                .split(" ")
-                .map(str::parse)
-                .map(Result::unwrap)
-                .collect(),
-            seed_to_soil: Projection::parse(captures.get(2).unwrap().as_str()),
-            soil_to_fertilizer: Projection::parse(captures.get(3).unwrap().as_str()),
-            fertilizer_to_water: Projection::parse(captures.get(4).unwrap().as_str()),
-            water_to_light: Projection::parse(captures.get(5).unwrap().as_str()),
-            light_to_temperature: Projection::parse(captures.get(6).unwrap().as_str()),
-            temperature_to_humidity: Projection::parse(captures.get(7).unwrap().as_str()),
-            humidity_to_location: Projection::parse(captures.get(8).unwrap().as_str()),
-        }
+        let sections = double_line_ending.split(input).collect_vec();
+
+        let seeds: Vec<i64> = sections
+            .first()
+            .unwrap()
+            .trim_start_matches("seeds: ")
+            .split(" ")
+            .map(str::parse)
+            .flatten()
+            .collect_vec();
+
+        let projections = sections
+            .iter()
+            .skip(1)
+            .map(|section: &&str| Projection::parse(&section))
+            .collect_vec();
+
+        Self { seeds, projections }
     }
 
-    pub fn get_seed_to_location_chain(&self) -> Vec<&Projection> {
-        vec![
-            &self.seed_to_soil,
-            &self.soil_to_fertilizer,
-            &self.fertilizer_to_water,
-            &self.water_to_light,
-            &self.light_to_temperature,
-            &self.temperature_to_humidity,
-            &self.humidity_to_location,
-        ]
+    fn map_to(&self, source: i64) -> i64 {
+        self.projections.iter().fold(source, |i, p| p.map_to(i))
+    }
+
+    fn map_ranges(&self, ranges: Vec<Range>) -> Vec<Range> {
+        self.projections
+            .iter()
+            .fold(ranges, |i, p: &Projection| p.map_ranges(i))
     }
 }
 
-pub fn part1(input: String) {
+pub fn part1(input: &str) -> i64 {
     let almanac = Almanac::parse(input);
-    let chain = almanac.get_seed_to_location_chain();
 
     let result = almanac
         .seeds
         .iter()
-        .map(|s| chain.map_to(*s))
+        .map(|s| almanac.map_to(*s))
         .min()
         .unwrap();
 
-    println!("Result: {:?}", result);
+    return result;
 }
 
-pub fn part2(input: String) {
+pub fn part2(input: &str) -> i64 {
     let almanac = Almanac::parse(input);
-    let chain = almanac.get_seed_to_location_chain();
 
     let seed_ranges: Vec<Range> = almanac
         .seeds
@@ -269,17 +227,20 @@ pub fn part2(input: String) {
             start: w[0],
             length: w[1],
         })
-        .take(1) // Take this out for a sharp run
         .collect();
 
-    let result = chain.map_ranges(seed_ranges).iter().map(|r| r.start).min();
+    let result = almanac
+        .map_ranges(seed_ranges)
+        .iter()
+        .map(|r| r.start)
+        .min()
+        .unwrap();
 
-    println!("Result: {:?}", result);
+    return result;
 }
 
-pub fn part2_bf(input: String) {
+pub fn part2_brute_force(input: &str) -> i64 {
     let almanac = Almanac::parse(input);
-    let chain = almanac.get_seed_to_location_chain();
 
     let seeds: Vec<i64> = almanac
         .seeds
@@ -287,44 +248,80 @@ pub fn part2_bf(input: String) {
         .flat_map(|w| w[0]..(w[0] + w[1]))
         .collect();
 
-    let result = seeds.iter().map(|s| chain.map_to(*s)).min().unwrap();
+    let result = seeds.iter().map(|s| almanac.map_to(*s)).min().unwrap();
 
-    println!("Result: {:?}", result);
+    return result;
 }
 
 pub fn process(input: String) {
-    part2_bf(input);
+    use std::time::Instant;
+    let now = Instant::now();
+    let result = part2(&input);
+    println!("Result: {:?}", result);
+    println!("Finished in: {:.2?}", now.elapsed());
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const EXAMPLE: &str = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
     #[test]
-    fn soil_mappings() {
-        let almanac = Almanac::parse(include_str!("example.txt").to_string());
-        assert_eq!(almanac.seed_to_soil.map_to(79), 81);
-        assert_eq!(almanac.seed_to_soil.map_to(14), 14);
-        assert_eq!(almanac.seed_to_soil.map_to(55), 57);
-        assert_eq!(almanac.seed_to_soil.map_to(13), 13);
+    fn part1_example() {
+        let result = part1(EXAMPLE);
+        assert_eq!(result, 35);
     }
 
     #[test]
-    fn location_mappings() {
-        let almanac = Almanac::parse(include_str!("example.txt").to_string());
-        let chain = almanac.get_seed_to_location_chain();
-
-        assert_eq!(chain.map_to(79), 82);
-        assert_eq!(chain.map_to(14), 43);
-        assert_eq!(chain.map_to(55), 86);
-        assert_eq!(chain.map_to(13), 35);
+    fn part1_input() {
+        let input = include_str!("input.txt");
+        let result = part1(input);
+        assert_eq!(result, 579439039);
     }
 
     #[test]
-    fn part2_solution() {
-        let almanac = Almanac::parse(include_str!("example.txt").to_string());
-        let chain = almanac.get_seed_to_location_chain();
+    fn part2_example() {
+        let result = part2(EXAMPLE);
+        assert_eq!(result, 46);
+    }
 
-        assert_eq!(chain.map_to(82), 46);
+    #[test]
+    fn part2_input() {
+        let input = include_str!("input.txt");
+        let result = part2(input);
+        assert_eq!(result, 7873084);
     }
 }
