ceba19de1801036b7a58d38833346b6b02f66d8e
Dryk, Jan <jan.dryk@merck.com>
2023-12-14 23:40:21 +0100

    refactor and optimise day 6

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,16 +1,12 @@
 use indicatif::ProgressIterator;
+use itertools::Itertools;
 use nom::{
-    branch::alt,
-    bytes::complete::{escaped, tag, take_while},
-    character::complete::{
-        alphanumeric1 as alphanumeric, char, digit1, line_ending, multispace1, one_of,
-    },
-    combinator::{cut, map, map_res, opt, value},
-    error::{context, convert_error, ContextError, ErrorKind, ParseError, VerboseError},
-    multi::{separated_list0, separated_list1},
-    number::complete::u32,
-    sequence::{delimited, pair, preceded, separated_pair, terminated},
-    Err, IResult, Parser,
+    bytes::complete::tag,
+    character::complete::{digit1, line_ending, multispace1},
+    combinator::map_res,
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult, Parser,
 };
 
 pub fn parse(input: &str) -> (Vec<u64>, Vec<u64>) {
@@ -35,67 +31,120 @@ pub fn parse(input: &str) -> (Vec<u64>, Vec<u64>) {
     return result;
 }
 
-pub fn part1(input: String) {
-    let (times, distances) = parse(&input);
-
-    let races = times.iter().zip(distances.iter());
-
-    let result: Vec<usize> = races
-        
-        .map(|(time, limit)| {
-            (1..*time)
-                .map(|hold| {
-                    let speed = hold;
-                    let time_remaining = time - hold;
-                    let distance = speed * time_remaining;
-                    // println!("Hold for {hold}, distance {distance} limit {limit}");
-                    return distance;
-                })
-                .filter(|a| a > limit)
-                .count()
+fn winning_races_brute_force(time: u64, record_distance: u64) -> usize {
+    let result = (1..time)
+        .progress_count(time)
+        .map(|hold| {
+            let speed = hold;
+            let time_remaining = time - hold;
+            let distance = speed * time_remaining;
+            // println!("Hold for {hold}, distance {distance} limit {limit}");
+            return distance;
         })
-        .collect();
-    
-    println!("{:?}", result);
-    println!("Result: {:?}", result.iter().product::<usize>());
+        .filter(|a| a > &record_distance)
+        .count();
+
+    return result;
 }
 
-pub fn part2(input: String) {
+/// distance traveled is equal to total time - time pressed, times time pressed
+///
+/// total time: t
+/// time pressed: x
+/// recordDistance = d
+///
+/// (t - x) * x = d => -x^2 + xt - d = 0
+///
+/// equation solutions are min and max time pressed to beat record
+fn winning_races_equation(time: u64, record_distance: u64) -> usize {
+    let t = time as f64;
+    let d = record_distance as f64;
+
+    // * D = b^2 - 4ac
+    //  * so, t^2 - 4d
+    let D_root = (t * t - 4. * d).sqrt();
+
+    // * x_1,2 = (-b +- sqrt of D)/2
+    let a = (t - D_root) / 2.;
+    let b = (t + D_root) / 2.;
+
+    let min_time = (a + 0.0001).ceil() as usize;
+    let max_time = (b - 0.0001).floor() as usize;
+
+    return max_time - min_time + 1;
+}
+
+pub fn part1(input: &str) -> usize {
     let (times, distances) = parse(&input);
 
-    let races = times.iter().zip(distances.iter());
+    let races = times
+        .iter()
+        .zip(distances.iter())
+        .map(|(time, distance)| (*time, *distance))
+        .collect_vec();
 
-    let result: Vec<usize> = races
-        
-        .map(|(time, limit)| {
-            (1..*time)
-                .progress_count(*time)
-                .map(|hold| {
-                    let speed = hold;
-                    let time_remaining = time - hold;
-                    let distance = speed * time_remaining;
-                    // println!("Hold for {hold}, distance {distance} limit {limit}");
-                    return distance;
-                })
-                .filter(|a| a > limit)
-                .count()
-        })
-        .collect();
-    
-    println!("{:?}", result);
-    println!("Result: {:?}", result.iter().product::<usize>());
+    let result = races
+        .iter()
+        .map(|(t, d)| winning_races_equation(*t, *d))
+        .product::<usize>();
+
+    return result;
+}
+
+fn part2(input: &str) -> usize {
+    let (times, distances) = parse(&input);
+
+    let time: u64 = times.iter().join("").parse().unwrap();
+    let distance: u64 = distances.iter().join("").parse().unwrap();
+
+    let races = vec![(time, distance)];
+
+    let result = races
+        .iter()
+        .map(|(t, d)| winning_races_equation(*t, *d))
+        .product::<usize>();
+
+    return result;
 }
 
 pub fn process(input: String) {
-    part2(input);
+    use std::time::Instant;
+    let now = Instant::now();
+    let result = part2(&input);
+    println!("Result: {result}");
+    println!("Finished in: {:.2?}", now.elapsed());
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const EXAMPLE: &str = "Time:      7  15   30
+Distance:  9  40  200";
+
     #[test]
-    fn part1_example_test() {
-        part1(include_str!("example.txt").to_string())
+    fn part1_example() {
+        let result = part1(EXAMPLE);
+        assert_eq!(result, 288);
+    }
+
+    #[test]
+    fn part1_input() {
+        let input = include_str!("input.txt");
+        let result = part1(input);
+        assert_eq!(result, 500346);
+    }
+
+    #[test]
+    fn part2_example() {
+        let result = part2(EXAMPLE);
+        assert_eq!(result, 71503);
+    }
+
+    #[test]
+    fn part2_input() {
+        let input = include_str!("input.txt");
+        let result = part2(input);
+        assert_eq!(result, 42515755);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::{env, fs, path::Path};
 
-mod day14;
+mod day6;
 
 fn main() {
     let _args: Vec<String> = env::args().collect();
-    let default_filename = Path::new("src/day14/input.txt");
+    let default_filename = Path::new("src/day6/input.txt");
     // let filename = args.get(1).unwrap_or(&default_filename);
 
     let input = fs::read_to_string(default_filename).expect("Wrong file location");
 
-    day14::process(input);
+    day6::process(input);
 }
