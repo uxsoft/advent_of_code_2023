fa2b122abdc901090d97c57b7a6a1fa54c116cfe
Dryk, Jan <jan.dryk@merck.com>
2023-12-18 12:41:45 +0100

    day 17

--- src/day17/mod.rs
+++ src/day17/mod.rs
@@ -24,13 +24,13 @@ enum Direction {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-struct Node {
+struct Crucible {
     pos: IVec2,
     dir: Direction,
     n_straight: u8,
 }
 
-impl Node {
+impl Crucible {
     fn new(x: i32, y: i32, dir: Direction, n_straight: u8) -> Self {
         Self {
             pos: IVec2::new(x, y),
@@ -38,78 +38,189 @@ impl Node {
             n_straight,
         }
     }
+}
 
-    fn successors(&self) -> Vec<Node> {
+struct LargeCrucibleMechanics;
+struct UltraCrucibleMechanics;
+
+trait CrucibleMechanics {
+    fn successors(&self, node: &Crucible) -> Vec<Crucible>;
+
+    fn starts(&self) -> Vec<Crucible>;
+
+    fn can_stop(&self, node: &Crucible) -> bool;
+}
+
+impl CrucibleMechanics for LargeCrucibleMechanics {
+    fn successors(&self, node: &Crucible) -> Vec<Crucible> {
         use Direction::*;
 
-        let mut result: Vec<Node> = Vec::new();
+        const MAX_N_STRAIGHT: u8 = 3;
+
+        let mut result: Vec<Crucible> = Vec::new();
 
         // Try add Down
-        if self.dir != Up && !(self.dir == Down && self.n_straight >= 3) {
-            let m_straight = if self.dir == Down {
-                self.n_straight + 1
+        if node.dir != Up && !(node.dir == Down && node.n_straight >= MAX_N_STRAIGHT) {
+            let m_straight = if node.dir == Down {
+                node.n_straight + 1
             } else {
                 1
             };
-            result.push(Node::new(self.pos.x, self.pos.y + 1, Down, m_straight));
+            result.push(Crucible::new(node.pos.x, node.pos.y + 1, Down, m_straight));
         }
 
         // Try add Up
-        if self.dir != Down && self.pos.y > 0 && !(self.dir == Up && self.n_straight >= 3) {
-            let m_straight = if self.dir == Up {
-                self.n_straight + 1
+        if node.dir != Down
+            && node.pos.y > 0
+            && !(node.dir == Up && node.n_straight >= MAX_N_STRAIGHT)
+        {
+            let m_straight = if node.dir == Up {
+                node.n_straight + 1
             } else {
                 1
             };
-            result.push(Node::new(self.pos.x, self.pos.y - 1, Up, m_straight));
+            result.push(Crucible::new(node.pos.x, node.pos.y - 1, Up, m_straight));
         }
 
         // Try add Right
-        if self.dir != Left && !(self.dir == Right && self.n_straight >= 3) {
-            let m_straight = if self.dir == Right {
-                self.n_straight + 1
+        if node.dir != Left && !(node.dir == Right && node.n_straight >= MAX_N_STRAIGHT) {
+            let m_straight = if node.dir == Right {
+                node.n_straight + 1
             } else {
                 1
             };
-            result.push(Node::new(self.pos.x + 1, self.pos.y, Right, m_straight));
+            result.push(Crucible::new(node.pos.x + 1, node.pos.y, Right, m_straight));
         }
 
         // Try add Left
-        if self.dir != Right && self.pos.x > 0 && !(self.dir == Left && self.n_straight >= 3) {
-            let m_straight = if self.dir == Left {
-                self.n_straight + 1
+        if node.dir != Right
+            && node.pos.x > 0
+            && !(node.dir == Left && node.n_straight >= MAX_N_STRAIGHT)
+        {
+            let m_straight = if node.dir == Left {
+                node.n_straight + 1
             } else {
                 1
             };
-            result.push(Node::new(self.pos.x - 1, self.pos.y, Left, m_straight));
+            result.push(Crucible::new(node.pos.x - 1, node.pos.y, Left, m_straight));
         }
 
         return result;
     }
+
+    fn starts(&self) -> Vec<Crucible> {
+        vec![Crucible::new(0, 0, Direction::Right, 0)]
+    }
+
+    fn can_stop(&self, node: &Crucible) -> bool {
+        true
+    }
 }
 
-fn find_shortest_path(grid: &HashMap<IVec2, u32>) -> (Vec<Node>, u32) {
+impl CrucibleMechanics for UltraCrucibleMechanics {
+    fn successors(&self, node: &Crucible) -> Vec<Crucible> {
+        use Direction::*;
+        const MAX_N_STRAIGHT: u8 = 10;
+        const MIN_N_STRAIGHT: u8 = 4;
+
+        let mut result: Vec<Crucible> = Vec::new();
+
+        if node.n_straight < MIN_N_STRAIGHT {
+            result.push(match node.dir {
+                Up => Crucible::new(node.pos.x, node.pos.y - 1, Up, node.n_straight + 1),
+                Down => Crucible::new(node.pos.x, node.pos.y + 1, Down, node.n_straight + 1),
+                Right => Crucible::new(node.pos.x + 1, node.pos.y, Right, node.n_straight + 1),
+                Left => Crucible::new(node.pos.x - 1, node.pos.y, Left, node.n_straight + 1),
+            })
+        } else {
+            // Try add Down
+            if node.dir != Up && !(node.dir == Down && node.n_straight >= MAX_N_STRAIGHT) {
+                let m_straight = if node.dir == Down {
+                    node.n_straight + 1
+                } else {
+                    1
+                };
+                result.push(Crucible::new(node.pos.x, node.pos.y + 1, Down, m_straight));
+            }
+
+            // Try add Up
+            if node.dir != Down
+                && node.pos.y > 0
+                && !(node.dir == Up && node.n_straight >= MAX_N_STRAIGHT)
+            {
+                let m_straight = if node.dir == Up {
+                    node.n_straight + 1
+                } else {
+                    1
+                };
+                result.push(Crucible::new(node.pos.x, node.pos.y - 1, Up, m_straight));
+            }
+
+            // Try add Right
+            if node.dir != Left && !(node.dir == Right && node.n_straight >= MAX_N_STRAIGHT) {
+                let m_straight = if node.dir == Right {
+                    node.n_straight + 1
+                } else {
+                    1
+                };
+                result.push(Crucible::new(node.pos.x + 1, node.pos.y, Right, m_straight));
+            }
+
+            // Try add Left
+            if node.dir != Right
+                && node.pos.x > 0
+                && !(node.dir == Left && node.n_straight >= MAX_N_STRAIGHT)
+            {
+                let m_straight = if node.dir == Left {
+                    node.n_straight + 1
+                } else {
+                    1
+                };
+                result.push(Crucible::new(node.pos.x - 1, node.pos.y, Left, m_straight));
+            }
+        }
+
+        return result;
+    }
+
+    fn starts(&self) -> Vec<Crucible> {
+        vec![
+            Crucible::new(0, 0, Direction::Right, 0),
+            Crucible::new(0, 0, Direction::Down, 0),
+        ]
+    }
+
+    fn can_stop(&self, node: &Crucible) -> bool {
+        node.n_straight >= 4
+    }
+}
+
+fn find_shortest_path(
+    grid: &HashMap<IVec2, u32>,
+    start: Crucible,
+    mechanics: &impl CrucibleMechanics,
+) -> (Vec<Crucible>, u32) {
     let max_x = grid.keys().map(|i| i.x).max().unwrap();
     let max_y = grid.keys().map(|i| i.y).max().unwrap();
 
-    let start = Node::new(0, 0, Direction::Right, 0);
     let goal_pos = IVec2::new(max_x, max_y);
     let (path, distance) = dijkstra(
         &start,
         |node| {
-            node.successors()
+            mechanics
+                .successors(node)
                 .iter()
                 .filter_map(|i| grid.get(&i.pos).map(|cost| (*i, *cost)))
-                .collect::<Vec<(Node, u32)>>()
+                .collect::<Vec<(Crucible, u32)>>()
         },
-        |node| node.pos == goal_pos,
+        |node| node.pos == goal_pos && mechanics.can_stop(node),
     )
     .expect("should have a valid path");
 
     return (path, distance);
 }
 
-fn print_path(path: &Vec<Node>) -> String {
+fn print_path(path: &Vec<Crucible>) -> String {
     let max_x = path.iter().map(|i| i.pos.x).max().unwrap();
     let max_y = path.iter().map(|i| i.pos.y).max().unwrap();
 
@@ -132,15 +243,35 @@ fn print_path(path: &Vec<Node>) -> String {
 pub fn part1(input: &str) -> u32 {
     let grid = parse(input);
 
-    let (p, d) = find_shortest_path(&grid);
+    let mechanics = LargeCrucibleMechanics {};
 
-    println!("{}", print_path(&p));
+    let (p, d) = &mechanics
+        .starts()
+        .iter()
+        .map(|start| find_shortest_path(&grid, *start, &mechanics))
+        .min_by_key(|(_, d)| *d)
+        .unwrap();
 
-    return d;
+    // println!("{}", print_path(p));
+
+    return *d;
 }
 
-pub fn part2(input: &str) -> usize {
-    return 0;
+pub fn part2(input: &str) -> u32 {
+    let grid = parse(input);
+
+    let mechanics = UltraCrucibleMechanics {};
+
+    let (p, d) = &mechanics
+        .starts()
+        .iter()
+        .map(|start| find_shortest_path(&grid, *start, &mechanics))
+        .min_by_key(|(_, d)| *d)
+        .unwrap();
+
+    // println!("{}", print_path(p));
+
+    return *d;
 }
 
 pub fn process(input: String) {
@@ -182,17 +313,27 @@ mod tests {
         assert_eq!(result, 843);
     }
 
-    // #[test]
+    #[test]
     fn part2_example() {
-        let input = "";
         let result = part2(EXAMPLE);
-        assert_eq!(result, 0);
+        assert_eq!(result, 94);
     }
 
-    // #[test]
+    #[test]
+    fn part2_example2() {
+        let input = "111111111111
+999999999991
+999999999991
+999999999991
+999999999991";
+        let result = part2(input);
+        assert_eq!(result, 71);
+    }
+
+    #[test]
     fn part2_input() {
         let input = include_str!("input.txt");
         let result = part2(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 1017);
     }
 }
