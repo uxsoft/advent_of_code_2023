13708ce4fad2badd1b5d34715fb6d3d1c194c161
Jan Dryk <me@uxsoft.cz>
2023-12-20 19:25:01 +0100

    part2 before refactoring

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -1,11 +1,15 @@
 use itertools::Itertools;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, VecDeque};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum ModuleKind {
     Broadcaster,
     Conjunction,
     FlipFlop,
+}
+
+impl ModuleKind {
+    fn process() {}
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -36,10 +40,20 @@ fn parse(input: &str) -> (BTreeMap<&str, ModuleKind>, BTreeMap<&str, Vec<&str>>)
     return (kinds, destinations);
 }
 
+fn extract_result(factors: BTreeMap<&str, Vec<usize>>) -> usize {
+    let first_encounters = factors.values().map(|v| *v.first().unwrap()).collect_vec();
+    dbg!(factors);
+    println!("Got: {:?}", first_encounters);
+    println!("Expected: 3917, 3919, 4007, 4027");
+    let res2 = reikna::factor::lcm_all(&first_encounters.iter().map(|i| *i as u64).collect_vec());
+
+    return 0;//res2 as usize;
+}
+
 pub fn part2(input: &str) -> usize {
     let (kinds, destinations) = parse(input);
 
-    let mut queue: Vec<(&str, &str, SignalKind)> = vec![];
+    let mut queue: VecDeque<(&str, &str, SignalKind)> = VecDeque::new();
 
     let mut ff_memory = BTreeMap::new();
     let mut con_memory: BTreeMap<&str, BTreeMap<&str, SignalKind>> = destinations
@@ -55,17 +69,13 @@ pub fn part2(input: &str) -> usize {
         })
         .collect();
 
-    for i in 1..100000000 {
-        queue.push(("button", "broadcaster", SignalKind::Low));
+    let mut factors: BTreeMap<&str, Vec<usize>> = BTreeMap::new();
 
-        while let Some((sender, receiver, signal)) = queue.pop() {
+    for i in 1.. {
+        queue.push_back(("button", "broadcaster", SignalKind::Low));
+
+        while let Some((sender, receiver, signal)) = queue.pop_front() {
             // println!("{sender} -{signal:?}-> `{receiver}`");
-
-            for mon in vec!["mp", "qt", "qb"] {
-                if receiver == mon && signal == SignalKind::High {
-                    println!("{i}: Encountered HIGH to {mon}")
-                }
-            }
 
             if !kinds.contains_key(receiver) {
                 if signal == SignalKind::Low {
@@ -75,10 +85,24 @@ pub fn part2(input: &str) -> usize {
                 }
             }
 
+            for mon in vec!["mp", "qt", "qb", "ng"] {
+                if receiver == mon && signal == SignalKind::Low {
+                    println!("{i}: Encountered LOW to {mon}");
+                    factors
+                        .entry(receiver)
+                        .and_modify(|v| v.push(i))
+                        .or_insert(vec![i]);
+                }
+            }
+
+            if factors.len() == 4 && factors.values().all(|v| v.len() > 2) {
+                return extract_result(factors);
+            }
+
             match kinds.get(receiver).unwrap() {
                 ModuleKind::Broadcaster => {
                     for destination in &destinations[receiver] {
-                        queue.push((receiver, destination, signal));
+                        queue.push_back((receiver, destination, signal));
                     }
                 }
                 ModuleKind::FlipFlop => {
@@ -87,13 +111,13 @@ pub fn part2(input: &str) -> usize {
                         (false, SignalKind::Low) => {
                             ff_memory.insert(receiver, true);
                             for destination in &destinations[receiver] {
-                                queue.push((receiver, destination, SignalKind::High));
+                                queue.push_back((receiver, destination, SignalKind::High));
                             }
                         }
                         (true, SignalKind::Low) => {
                             ff_memory.insert(receiver, false);
                             for destination in &destinations[receiver] {
-                                queue.push((receiver, destination, SignalKind::Low));
+                                queue.push_back((receiver, destination, SignalKind::Low));
                             }
                         }
                         (_, SignalKind::High) => (), // High pulse when off is ignored
@@ -105,7 +129,7 @@ pub fn part2(input: &str) -> usize {
 
                     let all_high = state.values().all(|v| v == &SignalKind::High);
                     for destination in &destinations[receiver] {
-                        queue.push((
+                        queue.push_back((
                             receiver,
                             destination,
                             if all_high {
@@ -120,13 +144,12 @@ pub fn part2(input: &str) -> usize {
         }
     }
 
+    dbg!(&factors);
     return 1;
 }
 
 pub fn part1(input: &str) -> usize {
     let (kinds, destinations) = parse(input);
-
-    dbg!(&kinds);
 
     let mut queue: Vec<(&str, &str, SignalKind)> = vec![];
 
@@ -223,7 +246,7 @@ pub fn process(input: String) {
 mod tests {
     use super::*;
 
-    // #[test]
+    #[test]
     fn part1_example() {
         let result = part1(
             "broadcaster -> a, b, c
@@ -235,7 +258,7 @@ mod tests {
         assert_eq!(result, 32000000);
     }
 
-    // #[test]
+    #[test]
     fn part1_example2() {
         let result = part1(
             "broadcaster -> a
@@ -247,23 +270,17 @@ mod tests {
         assert_eq!(result, 11687500);
     }
 
-    // #[test]
+    #[test]
     fn part1_input() {
         let input = include_str!("input.txt");
         let result = part1(input);
         assert_eq!(result, 919383692);
     }
 
-    // #[test]
-    // fn part2_example() {
-    //     let result = part2(EXAMPLE);
-    //     assert_eq!(result, 0);
-    // }
-
     #[test]
     fn part2_input() {
         let input = include_str!("input.txt");
         let result = part2(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 247702167614647);
     }
 }
