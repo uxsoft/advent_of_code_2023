710a63368fbe5a86a193a48e2fe947c8891003f4
Jan Dryk <me@uxsoft.cz>
2023-12-21 16:52:01 +0100

    day 21 bench

--- benches/aoc_all.rs
+++ benches/aoc_all.rs
@@ -8,7 +8,7 @@ mod day1;
 
 #[divan::bench]
 fn day1() {
-    day1::part2(divan::black_box(include_str!("../src/day1/input.txt",)));
+    day1::part2(divan::black_box(include_str!("../src/day1/input.txt")));
 }
 
 #[path = "../src/day2/mod.rs"]
@@ -16,7 +16,7 @@ mod day2;
 
 #[divan::bench]
 fn day2() {
-    day2::part2(divan::black_box(include_str!("../src/day2/input.txt",)));
+    day2::part2(divan::black_box(include_str!("../src/day2/input.txt")));
 }
 
 #[path = "../src/day3/mod.rs"]
@@ -24,7 +24,7 @@ mod day3;
 
 #[divan::bench]
 fn day3() {
-    day3::part2(divan::black_box(include_str!("../src/day3/input.txt",)));
+    day3::part2(divan::black_box(include_str!("../src/day3/input.txt")));
 }
 
 #[path = "../src/day4/mod.rs"]
@@ -32,7 +32,7 @@ mod day4;
 
 #[divan::bench]
 fn day4() {
-    day4::part2(divan::black_box(include_str!("../src/day4/input.txt",)));
+    day4::part2(divan::black_box(include_str!("../src/day4/input.txt")));
 }
 
 #[path = "../src/day5/mod.rs"]
@@ -40,7 +40,7 @@ mod day5;
 
 #[divan::bench]
 fn day5() {
-    day5::part2(divan::black_box(include_str!("../src/day5/input.txt",)));
+    day5::part2(divan::black_box(include_str!("../src/day5/input.txt")));
 }
 
 #[path = "../src/day6/mod.rs"]
@@ -48,7 +48,7 @@ mod day6;
 
 #[divan::bench]
 fn day6() {
-    day6::part2(divan::black_box(include_str!("../src/day6/input.txt",)));
+    day6::part2(divan::black_box(include_str!("../src/day6/input.txt")));
 }
 
 #[path = "../src/day7/mod.rs"]
@@ -56,7 +56,7 @@ mod day7;
 
 #[divan::bench]
 fn day7() {
-    day7::part2(divan::black_box(include_str!("../src/day7/input.txt",)));
+    day7::part2(divan::black_box(include_str!("../src/day7/input.txt")));
 }
 
 #[path = "../src/day8/mod.rs"]
@@ -64,7 +64,7 @@ mod day8;
 
 #[divan::bench]
 fn day8() {
-    day8::part2(divan::black_box(include_str!("../src/day8/input.txt",)));
+    day8::part2(divan::black_box(include_str!("../src/day8/input.txt")));
 }
 
 #[path = "../src/day9/mod.rs"]
@@ -72,7 +72,7 @@ mod day9;
 
 #[divan::bench]
 fn day9() {
-    day9::part2(divan::black_box(include_str!("../src/day9/input.txt",)));
+    day9::part2(divan::black_box(include_str!("../src/day9/input.txt")));
 }
 
 #[path = "../src/day10/mod.rs"]
@@ -80,7 +80,7 @@ mod day10;
 
 #[divan::bench]
 fn day10() {
-    day10::part2(divan::black_box(include_str!("../src/day10/input.txt",)));
+    day10::part2(divan::black_box(include_str!("../src/day10/input.txt")));
 }
 
 #[path = "../src/day11/mod.rs"]
@@ -89,7 +89,7 @@ mod day11;
 #[divan::bench]
 fn day11() {
     day11::part2(
-        divan::black_box(include_str!("../src/day11/input.txt",)),
+        divan::black_box(include_str!("../src/day11/input.txt")),
         1_000_000,
     );
 }
@@ -99,7 +99,7 @@ mod day12;
 
 #[divan::bench]
 fn day12() {
-    day12::part2(divan::black_box(include_str!("../src/day12/input.txt",)));
+    day12::part2(divan::black_box(include_str!("../src/day12/input.txt")));
 }
 
 #[path = "../src/day13/mod.rs"]
@@ -107,7 +107,7 @@ mod day13;
 
 #[divan::bench]
 fn day13() {
-    day13::part2(divan::black_box(include_str!("../src/day13/input.txt",)));
+    day13::part2(divan::black_box(include_str!("../src/day13/input.txt")));
 }
 
 #[path = "../src/day14/mod.rs"]
@@ -115,7 +115,7 @@ mod day14;
 
 #[divan::bench]
 fn day14() {
-    day14::part2(divan::black_box(include_str!("../src/day14/input.txt",)));
+    day14::part2(divan::black_box(include_str!("../src/day14/input.txt")));
 }
 
 #[path = "../src/day15/mod.rs"]
@@ -123,7 +123,7 @@ mod day15;
 
 #[divan::bench]
 fn day15() {
-    day15::part2(divan::black_box(include_str!("../src/day15/input.txt",)));
+    day15::part2(divan::black_box(include_str!("../src/day15/input.txt")));
 }
 
 #[path = "../src/day16/mod.rs"]
@@ -131,7 +131,7 @@ mod day16;
 
 #[divan::bench]
 fn day16() {
-    day16::part2(divan::black_box(include_str!("../src/day16/input.txt",)));
+    day16::part2(divan::black_box(include_str!("../src/day16/input.txt")));
 }
 
 #[path = "../src/day17/mod.rs"]
@@ -139,7 +139,7 @@ mod day17;
 
 #[divan::bench]
 fn day17() {
-    day17::part2(divan::black_box(include_str!("../src/day17/input.txt",)));
+    day17::part2(divan::black_box(include_str!("../src/day17/input.txt")));
 }
 
 #[path = "../src/day18/mod.rs"]
@@ -147,7 +147,7 @@ mod day18;
 
 #[divan::bench]
 fn day18() {
-    day18::part2(divan::black_box(include_str!("../src/day18/input.txt",)));
+    day18::part2(divan::black_box(include_str!("../src/day18/input.txt")));
 }
 
 #[path = "../src/day19/mod.rs"]
@@ -155,7 +155,7 @@ mod day19;
 
 #[divan::bench]
 fn day19() {
-    day19::part2(divan::black_box(include_str!("../src/day19/input.txt",)));
+    day19::part2(divan::black_box(include_str!("../src/day19/input.txt")));
 }
 
 #[path = "../src/day20/mod.rs"]
@@ -163,5 +163,16 @@ mod day20;
 
 #[divan::bench]
 fn day20() {
-    day20::part2(divan::black_box(include_str!("../src/day20/input.txt",)));
+    day20::part2(divan::black_box(include_str!("../src/day20/input.txt")));
+}
+
+#[path = "../src/day21/mod.rs"]
+mod day21;
+
+#[divan::bench]
+fn day21() {
+    day21::part2(
+        divan::black_box(include_str!("../src/day21/input.txt")),
+        26501365,
+    );
 }
