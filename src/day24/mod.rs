73d79923ddc950d9cd89885a84f8c2dd4e8248db
Jan Dryk <me@uxsoft.cz>
2023-12-24 20:41:07 +0100

    part2 with z3 try

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,5 +1,6 @@
-use glam::{dvec2, DVec2, DVec3, Vec2};
-use itertools::Itertools;
+use glam::{dvec2, dvec3, DVec2, DVec3, Vec2};
+use itertools::{iproduct, Itertools};
+use z3::ast::Ast;
 
 #[derive(Debug)]
 struct Hailstone {
@@ -67,12 +68,41 @@ impl Hailstone {
         Some(dvec2(x, y))
     }
 
+    fn intersection_3d(&self, other: &Hailstone) -> Option<DVec3> {
+        let p = self.position; // P1
+        let v = self.trajectory; // v
+        let q = other.position; // Q1
+        let u = other.trajectory; // u
+
+        // find a = v x u
+        let a = v.cross(u); // cross product
+
+        // find dot product = (v x u) . (v x u)
+        let dot = a.dot(a); // inner product
+
+        // if v and u are parallel (v x u = 0), then no intersection, return NaN point
+        if dot == 0. {
+            return None;
+        }
+
+        // find b = (Q1-P1) x u
+        let b = (q - p).cross(u); // cross product
+
+        // find t = (b.a)/(a.a) = ((Q1-P1) x u) .(v x u) / (v x u) . (v x u)
+        let t = b.dot(a) / dot;
+
+        // find intersection point by substituting t to the line1 eq
+        let point = p + (t * v);
+
+        return Some(point);
+    }
+
     fn along_trajectory(&self, other: &DVec2) -> bool {
         if self.trajectory.x > 0. {
             return other.x > self.position.x;
         } else if self.trajectory.x < 0. {
             return other.x < self.position.x;
-        } 
+        }
 
         return false;
     }
@@ -100,14 +130,76 @@ pub fn part1(input: &str, from: i64, to: i64) -> usize {
     return intersections.len();
 }
 
+pub fn part2_bf(input: &str) -> usize {
+    let hail = parse(input);
+
+    let result = iproduct!(0.., 0.., 0.., 0.., 0.., 0..)
+        .find (|(px, py, pz, tx, ty, tz)| {
+        let this = Hailstone {
+            position: dvec3(*px as f64, *py as f64, *pz as f64),
+            trajectory: dvec3(*tx as f64, *ty as f64, *tz as f64),
+        };
+        let fits = hail.iter().take(3).all(|other| {
+            if let Some(intersection) = this.intersection_3d(&other) {
+                let t1 = this.position.distance(intersection) / this.trajectory;
+                let t2 = other.position.distance(intersection) / other.trajectory;
+
+                return t1 == t2;
+            } else {
+                return false;
+            }
+        });
+        return fits;
+    });
+
+    dbg!(&result);
+
+    return 0;
+}
+
 pub fn part2(input: &str) -> usize {
+    let hail = parse(input);
+
+    let cfg = z3::Config::new();
+    let context = z3::Context::new(&cfg);
+    let solver = z3::Solver::new(&context);
+
+    let x = z3::ast::Int::new_const(&context, "x");
+    let y = z3::ast::Int::new_const(&context, "y");
+    let z = z3::ast::Int::new_const(&context, "z");
+    let vx = z3::ast::Int::new_const(&context, "vx");
+    let vy = z3::ast::Int::new_const(&context, "vy");
+    let vz = z3::ast::Int::new_const(&context, "vz");
+
+    for (i, hs) in hail.iter().take(3).enumerate() {
+        let a = z3::ast::Int::from_i64(&context, hs.position.x);
+        let va = z3::ast::Int::from_i64(&context, hs.trajectory.x);
+        let b = z3::ast::Int::from_i64(&context, hs.position.y);
+        let vb = z3::ast::Int::from_i64(&context, hs.trajectory.y);
+        let c = z3::ast::Int::from_i64(&context, hs.position.z);
+        let vc = z3::ast::Int::from_i64(&context, hs.trajectory.z);
+
+        let t_i = z3::ast::Int::new_const(&context, format!("t{i}"));
+        solver.assert(&t_i.gt(&z3::ast::Int::from_i64(&context, 0)));
+        solver.assert(&(x.clone() + vx.clone() * t_i.clone())._eq(&(a + va * t_i.clone())));
+        solver.assert(&(y.clone() + vy.clone() * t_i.clone())._eq(&(b + vb * t_i.clone())));
+        solver.assert(&(z.clone() + vz.clone() * t_i.clone())._eq(&(c + vc * t_i.clone())));
+    }
+    if solver.check() == z3::SatResult::Sat {
+        let Some(m) = solver.get_model() else {
+            println!("Failed to solve!");
+            return;
+        };
+        println!("{}", m.eval(&(x + y + z), true).unwrap());
+    }
+
     return 0;
 }
 
 pub fn process(input: String) {
     use std::time::Instant;
     let now = Instant::now();
-    let result = part1(&input, 200000000000000, 400000000000000);
+    let result = part2(&input);
     println!("Result: {result}");
     println!("Finished in: {:.2?}", now.elapsed());
 }
