06d7ca488de403c6239e05f5c699c4e6b83fdce5
Jan Dryk <me@uxsoft.cz>
2023-12-24 21:23:08 +0100

    part2 using z3

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,11 +1,12 @@
-use glam::{dvec2, dvec3, DVec2, DVec3, Vec2};
+use glam::{dvec2, dvec3, DVec2, DVec3, Vec2, I64Vec3};
 use itertools::{iproduct, Itertools};
-use z3::ast::Ast;
+use z3::{Config, Context, Solver};
+use z3::ast::{Ast, Int};
 
 #[derive(Debug)]
 struct Hailstone {
-    position: DVec3,
-    trajectory: DVec3,
+    position: I64Vec3,
+    velocity: I64Vec3,
 }
 
 impl Hailstone {
@@ -26,8 +27,8 @@ impl Hailstone {
             .collect_vec();
 
         Hailstone {
-            position: DVec3::new(p[0], p[1], p[2]),
-            trajectory: DVec3::new(t[0], t[1], t[2]),
+            position: I64Vec3::new(p[0], p[1], p[2]),
+            velocity: I64Vec3::new(t[0], t[1], t[2]),
         }
     }
 
@@ -39,18 +40,18 @@ impl Hailstone {
 
         // Calculate Slope:
         // m = (y2-y1)/(x2-x1)
-        // m = (p.y + t.y - p.y) / (p.x + t.x - p.x)
-        // m = t.y/t.x
+        // m = (p.y + v.y - p.y) / (p.x + v.x - p.x)
+        // m = v.y/v.x
 
-        let m1 = self.trajectory.y / self.trajectory.x;
-        let m2 = other.trajectory.y / other.trajectory.x;
+        let m1 = self.velocity.y as f64 / self.velocity.x as f64;
+        let m2 = other.velocity.y as f64 / other.velocity.x as f64;
 
         // Calculate b (y-intercept):
         // p.y = m*p.x + b
         // b = p.y - m*p.x
 
-        let b1 = self.position.y - m1 * self.position.x;
-        let b2 = other.position.y - m2 * other.position.x;
+        let b1 = self.position.y as f64 - m1 * self.position.x as f64;
+        let b2 = other.position.y as f64 - m2 * other.position.x as f64;
 
         // intersection, put the two equations against each other (at which x are the y equal):
         // y = m1*x + b1, y = m2*x + b2
@@ -68,40 +69,11 @@ impl Hailstone {
         Some(dvec2(x, y))
     }
 
-    fn intersection_3d(&self, other: &Hailstone) -> Option<DVec3> {
-        let p = self.position; // P1
-        let v = self.trajectory; // v
-        let q = other.position; // Q1
-        let u = other.trajectory; // u
-
-        // find a = v x u
-        let a = v.cross(u); // cross product
-
-        // find dot product = (v x u) . (v x u)
-        let dot = a.dot(a); // inner product
-
-        // if v and u are parallel (v x u = 0), then no intersection, return NaN point
-        if dot == 0. {
-            return None;
-        }
-
-        // find b = (Q1-P1) x u
-        let b = (q - p).cross(u); // cross product
-
-        // find t = (b.a)/(a.a) = ((Q1-P1) x u) .(v x u) / (v x u) . (v x u)
-        let t = b.dot(a) / dot;
-
-        // find intersection point by substituting t to the line1 eq
-        let point = p + (t * v);
-
-        return Some(point);
-    }
-
     fn along_trajectory(&self, other: &DVec2) -> bool {
-        if self.trajectory.x > 0. {
-            return other.x > self.position.x;
-        } else if self.trajectory.x < 0. {
-            return other.x < self.position.x;
+        if self.velocity.x > 0 {
+            return other.x > self.position.x as f64;
+        } else if self.velocity.x < 0 {
+            return other.x < self.position.x as f64;
         }
 
         return false;
@@ -130,70 +102,41 @@ pub fn part1(input: &str, from: i64, to: i64) -> usize {
     return intersections.len();
 }
 
-pub fn part2_bf(input: &str) -> usize {
-    let hail = parse(input);
-
-    let result = iproduct!(0.., 0.., 0.., 0.., 0.., 0..)
-        .find (|(px, py, pz, tx, ty, tz)| {
-        let this = Hailstone {
-            position: dvec3(*px as f64, *py as f64, *pz as f64),
-            trajectory: dvec3(*tx as f64, *ty as f64, *tz as f64),
-        };
-        let fits = hail.iter().take(3).all(|other| {
-            if let Some(intersection) = this.intersection_3d(&other) {
-                let t1 = this.position.distance(intersection) / this.trajectory;
-                let t2 = other.position.distance(intersection) / other.trajectory;
-
-                return t1 == t2;
-            } else {
-                return false;
-            }
-        });
-        return fits;
-    });
-
-    dbg!(&result);
-
-    return 0;
-}
-
 pub fn part2(input: &str) -> usize {
     let hail = parse(input);
 
-    let cfg = z3::Config::new();
-    let context = z3::Context::new(&cfg);
-    let solver = z3::Solver::new(&context);
+    let cfg = Config::new();
+    let ctx = Context::new(&cfg);
+    let solver = Solver::new(&ctx);
 
-    let x = z3::ast::Int::new_const(&context, "x");
-    let y = z3::ast::Int::new_const(&context, "y");
-    let z = z3::ast::Int::new_const(&context, "z");
-    let vx = z3::ast::Int::new_const(&context, "vx");
-    let vy = z3::ast::Int::new_const(&context, "vy");
-    let vz = z3::ast::Int::new_const(&context, "vz");
+    let px = Int::new_const(&ctx, "px");
+    let py = Int::new_const(&ctx, "py");
+    let pz = Int::new_const(&ctx, "pz");
+    let vx = Int::new_const(&ctx, "vx");
+    let vy = Int::new_const(&ctx, "vy");
+    let vz = Int::new_const(&ctx, "vz");
 
-    for (i, hs) in hail.iter().take(3).enumerate() {
-        let a = z3::ast::Int::from_i64(&context, hs.position.x);
-        let va = z3::ast::Int::from_i64(&context, hs.trajectory.x);
-        let b = z3::ast::Int::from_i64(&context, hs.position.y);
-        let vb = z3::ast::Int::from_i64(&context, hs.trajectory.y);
-        let c = z3::ast::Int::from_i64(&context, hs.position.z);
-        let vc = z3::ast::Int::from_i64(&context, hs.trajectory.z);
+    for hailstone in hail {
+        let pxn = Int::from_i64(&ctx, hailstone.position.x);
+        let pyn = Int::from_i64(&ctx, hailstone.position.y);
+        let pzn = Int::from_i64(&ctx, hailstone.position.z);
+        let vxn = Int::from_i64(&ctx, hailstone.velocity.x);
+        let vyn = Int::from_i64(&ctx, hailstone.velocity.y);
+        let vzn = Int::from_i64(&ctx, hailstone.velocity.z);
+        let tn = Int::fresh_const(&ctx, "t");
 
-        let t_i = z3::ast::Int::new_const(&context, format!("t{i}"));
-        solver.assert(&t_i.gt(&z3::ast::Int::from_i64(&context, 0)));
-        solver.assert(&(x.clone() + vx.clone() * t_i.clone())._eq(&(a + va * t_i.clone())));
-        solver.assert(&(y.clone() + vy.clone() * t_i.clone())._eq(&(b + vb * t_i.clone())));
-        solver.assert(&(z.clone() + vz.clone() * t_i.clone())._eq(&(c + vc * t_i.clone())));
-    }
-    if solver.check() == z3::SatResult::Sat {
-        let Some(m) = solver.get_model() else {
-            println!("Failed to solve!");
-            return;
-        };
-        println!("{}", m.eval(&(x + y + z), true).unwrap());
+        solver.assert(&(&pxn + &vxn * &tn)._eq(&(&px + &vx * &tn)));
+        solver.assert(&(&pyn + &vyn * &tn)._eq(&(&py + &vy * &tn)));
+        solver.assert(&(&pzn + &vzn * &tn)._eq(&(&pz + &vz * &tn)));
     }
 
-    return 0;
+    solver.check();
+    let model = solver.get_model().unwrap();
+    let x = model.get_const_interp(&px).unwrap().as_i64().unwrap();
+    let y = model.get_const_interp(&py).unwrap().as_i64().unwrap();
+    let z = model.get_const_interp(&pz).unwrap().as_i64().unwrap();
+
+    return (x + y + z).try_into().unwrap();
 }
 
 pub fn process(input: String) {
@@ -228,16 +171,16 @@ mod tests {
         assert_eq!(result, 16502);
     }
 
-    // #[test]
+    #[test]
     fn part2_example() {
         let result = part2(EXAMPLE);
-        assert_eq!(result, 0);
+        assert_eq!(result, 47);
     }
 
-    // #[test]
+    #[test]
     fn part2_input() {
         let input = include_str!("input.txt");
         let result = part2(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 673641951253289);
     }
 }
